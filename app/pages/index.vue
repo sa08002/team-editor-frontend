7449d32eff5a7d7d52d2c2946da4399c1e11d8f2
大西幹則 <1078sa08002@gmail.com>
2020-08-14 11:08:29 +0900

    TypScript を使用した書き方に変更

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,24 +1,26 @@
 <template>
- <div>
-  <ul v-for="article of articles" :key="article">
-   <li>タイトル : {{ article.title }}</li>
-   <li>記事内容 : {{ article.content }}</li>
-   <hr>
-  </ul>
- </div>
+  <div>
+    <ul v-for="article in articles" :key="article">
+      <li>タイトル : {{ article.title }}</li>
+      <hr>
+    </ul>
+  </div>
 </template>
 
 <script lang="ts">
- export default {
-  data: function() {
-   return {
-    articles: [
-     { title: "ruby 学習方法", content: "ruby はこのように勉強しましょう"},
-     { title: "PHP 学習方法", content: "PHP はこのように勉強しましょう" },
-     { title: "java 学習方法", content: "java はこのように勉強しましょう" },
-     { title: "HTML 学習方法", content: "HTML はこのように勉強しましょう" }
-    ]
-   }
-  }
- }
+import { Component, Vue } from 'nuxt-property-decorator';
+
+@Component
+export default class Articles extends Vue {
+  articles = [
+    {
+      title: "ruby 学習方法"
+    },
+    {
+      title: "PHP 学習方法"
+    },
+    {
+      title: "java 学習方法"
+    }]
+}
 </script>
