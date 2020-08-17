8c256f7acc2af9524c73b710254240a99c702aba
mc-chinju <takeiteasy.enjoyeverything@gmail.com>
2020-08-17 22:21:34 +0900

    user store に getter が準備されていなかったので追加

--- app/plugins/axios.js
+++ app/plugins/axios.js
@@ -1,7 +1,6 @@
 export default function setup({ store }) {
   store.$axios.interceptors.request.use((config) => {
-    let headers = {}
-    headers = store.getters['user/headers']
+    const headers = store.getters['user/headers']
     Object.keys(headers).forEach(function (key) {
       const value = headers[key]
       config.headers[key] = value
