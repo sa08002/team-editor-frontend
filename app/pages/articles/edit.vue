5555ec12b32db794f0d74d0b39728bdaed50dcf6
koniman210 <koniman1123@gmail.com>
2020-08-24 15:37:54 +0900

    編集ページの作成

---
+++ app/pages/articles/edit.vue
@@ -0,0 +1,122 @@
+<template>
+  <v-layout justify-center :class="$style.layout">
+    <v-flex :class="$style.container">
+      <h2 :class="$style.title">記事編集</h2>
+      <v-form ref="titleForm" @submit.prevent>
+        <v-text-field
+          v-model="title"
+          type="title"
+          name="name"
+          label="title"
+          data-vv-name="title"
+          solo
+          required
+          outlined
+          color="#212121"
+        />
+      </v-form>
+      <v-form ref="contentForm" @submit.prevent>
+        <v-textarea
+          v-model="content"
+          class="textarea"
+          type="text"
+          rows="5"
+          name="content"
+          label="本文"
+          hint=""
+          filled
+          counter
+          outlined
+          color="#212121"
+        />
+      </v-form>
+      <v-btn
+        color="cyan"
+        elevation="4"
+        ripple
+        block
+        depressed
+        :loading="loading"
+        class="white--text font-weight-bold"
+        :class="[$style.button, $style.register]"
+        @click="submit"
+        >更新する</v-btn
+      >
+    </v-flex>
+  </v-layout>
+</template>
+<script lang="ts">
+import { Vue, Component } from 'nuxt-property-decorator'
+import { Context } from '@nuxt/types'
+
+@Component({
+  async fetch(context: Context) {
+    const { store, error } = context
+    try {
+      await store.dispatch('article/editArticle')
+    } catch (err) {
+      error({
+        statusCode: err.response.status,
+        message: err.response.data.message,
+      })
+    }
+  },
+})
+export default class ArticleEditPage extends Vue {
+  title = this.article.title
+  content = this.article.content
+  loading = false
+  async submit(): Promise<void> {
+    const params = {
+      title: this.title,
+      content: this.content,
+    }
+    await this.$store.dispatch('article/updateArticle', params).then(() => {
+      this.$router.push('/')
+    })
+  }
+
+  get article() {
+    return this.$store.getters['article/article']
+  }
+}
+</script>
+<style lang="scss" module>
+.layout {
+  background: #f7f9f9;
+  .container {
+    height: 100%;
+    max-width: 360px;
+    padding: 20px 20px 60px;
+    background: white;
+  }
+  .title {
+    text-align: center;
+    margin-bottom: 20px;
+  }
+  .sign_in_link {
+    display: flex;
+    justify-content: center;
+    text-decoration: none;
+  }
+  .terms {
+    margin-right: 6px;
+  }
+  .register {
+    margin-top: 20px;
+  }
+  .divider {
+    margin: 20px 0;
+  }
+  .social_title {
+    margin-bottom: 20px;
+  }
+  .button {
+    margin-bottom: 14px;
+  }
+  .sns_text {
+    color: #ffffff;
+    margin-left: 8px;
+  }
+}
+</style>
