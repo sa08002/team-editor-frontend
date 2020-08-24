142a0658638ab88b280f59e88ef5ebbf0186a18b
koniman210 <koniman1123@gmail.com>
2020-08-24 15:36:18 +0900

    edit,update アクションの追加

--- app/store/article.js
+++ app/store/article.js
@@ -1,14 +1,19 @@
 export const state = () => ({
   articles: {},
+  article: {},
 })
 
 export const getters = {
   articles: (state) => state.articles,
+  article: (state) => state.article,
 }
 
 export const mutations = {
   setArticles(state, articles) {
     state.articles = articles
+  },
+  editArticle(state, article) {
+    state.article = article
   },
 }
 
@@ -21,5 +26,14 @@ export const actions = {
   },
   async articleNew({ commit }, params) {
     await this.$axios.post('/v1/articles', params).then(() => {})
+  },
+  async updateArticle({ commit }, params) {
+    await this.$axios.patch('/v1/articles/3', params).then(() => {})
+  },
+  async editArticle({ commit }, params) {
+    await this.$axios.get('/v1/articles/:id', params).then((response) => {
+      const article = response.data
+      commit('editArticle', article)
+    })
   },
 }
